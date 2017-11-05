1eae4d0233744094679889d5da8565e034a2a450
SeregaGomen <gserega71@gmail.com>
2017-11-05 13:20:31 +0200

    Last

--- test.py
+++ test.py
@@ -4,25 +4,29 @@
 from tkinter import *
 from tri_view import TTriView
 from tri_tri import TTri
+from time import time
 
 
 def main():
-    file_name = 'test/test2.rf'
+    file_name = 'test/test5.rf'
     tri = TTri()
 
     tri.set_file_name(file_name)
     tri.set_angle_optimize(True)
     tri.set_length_optimize(True)
+#    tri.set_step(250)
     tri.set_step(250)
-#    tri.set_step(600)
     tri.set_eps(1.0e-6)
     tri.set_angle(15)
     tri.set_ratio(1.5)
 
+    begin = time()
     if tri.start() is False:
         return
+    end = time()
+    print('calculation time: ', end - begin)
 
-    app = TTriView(Tk(), file_name, tri.x, tri.fe, tri.be, show_vertex=True, show_fe=True, show_be=True)
+    app = TTriView(Tk(), file_name, tri.x, tri.fe, tri.be, show_vertex=False, show_fe=False, show_be=True)
     app.mainloop()
 
 
